ad2458da50612305f8664f7e92097ab9515b7f7f
chlduq04 <chlduq04@gmail.com>
2015-07-14 22:29:09 +0900

    a

--- contents/js/init.js
+++ contents/js/init.js
@@ -2,12 +2,15 @@ function supportsImports() {
 	return 'import' in document.createElement('link');
 }
 
-if (supportsImports()) {
-	var link = document.querySelector('link[rel="import"]');
-	var content = link.import;
-	var el = content.querySelector('#common');
-	document.body.appendChild(el.cloneNode(true));
-} else {
-	alert("import를 지원하지 않습니다.");
-	window.location.href = "../index.html";
-}
+$(document).ready(function(){
+	if (supportsImports()) {
+		var link = document.querySelector('link[rel="import"]');
+		var content = link.import;
+		var el = content.querySelector('#common');
+		document.body.appendChild(el.cloneNode(true));
+	} else {
+		alert("import를 지원하지 않습니다.");
+		window.location.href = "../index.html";
+	}	
+})
+
